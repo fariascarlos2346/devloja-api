efd454e7599b2bc6f75699fded4096adc579fcef
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-11-07 00:24:10 -0300

    Banco de Dados configurado

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  part: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'devloja',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },  
+};
