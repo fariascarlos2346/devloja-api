d927bc77fd298b65dbab1e44f14864eef4089310
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-11-07 01:37:13 -0300

    estalado e importado o UUID

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
 
 import User from './app/models/User';
 
@@ -6,6 +7,7 @@ const routes = new Router();
 
 routes.get('/', async (request, response) => {
     const user = await User.create({
+        id: v4,
         name: 'Carlos',
         email: 'carlos@email.com',
         password_hash: 'assdflkhjkçkhlsdf',
