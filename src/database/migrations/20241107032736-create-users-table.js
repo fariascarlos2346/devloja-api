a90a2ebfa743dab6302e823d1340fef34c26bb47
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-11-07 00:48:05 -0300

    primeira magrate criada e o metodo down

--- src/database/migrations/20241107032736-create-users-table.js
+++ src/database/migrations/20241107032736-create-users-table.js
@@ -38,12 +38,7 @@ module.exports = {
     });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down (queryInterface) {
+    await queryInterface.dropTable('users');
+  },
 };
