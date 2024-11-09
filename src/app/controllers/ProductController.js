a96ce005e0650a10fc9801caa5f6249266ac45a2
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-11-09 13:00:16 -0300

    tabela criada com id unico no beekeeper

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import Product from '../models/Product';
 
 class ProductController {
     async store(request, response) {
@@ -14,7 +15,17 @@ class ProductController {
             return response.status(400).json({ error: err.errors });
         }
 
-        return response.status(201).json({ message: 'ok' })
+        const { filename: path } = request.file;
+        const { name, price, category } = request.body;
+
+        const product = await Product.create({
+            name,
+            price,
+            category,
+            path,
+        });
+
+        return response.status(201).json(product);
     }
 }
 
