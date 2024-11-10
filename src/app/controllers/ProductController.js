d50c1cb33f3d168387b3b83ff383f7f9db315342
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-11-10 11:30:45 -0300

    front end terminado, imagem sendo esposta no navegador com sucesso

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -27,6 +27,12 @@ class ProductController {
 
         return response.status(201).json(product);
     }
+
+    async index(request, response) {
+        const products = await Product.findAll();
+
+        return response.json(products);
+    }
 }
 
 export default new ProductController();
